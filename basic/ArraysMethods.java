37cb491522bee50249fb8d5a165bba4306ff5f1f
submish <subodhmishra015@gmail.com>
2024-10-28 22:12:32 +0545

    basic: array class functions like sort, equals, binary search, copyOf

--- basic/ArraysMethods.java
+++ basic/ArraysMethods.java
@@ -25,33 +25,34 @@ public class ArraysMethods {
         System.out.println("Position of 3: " + position); // binary search
 
         // int [] b = {1,2,3,4,5};
-        int [] b = Arrays.copyOf(a, a.length); // copy of array a
+        int [] bOne = Arrays.copyOf(a, a.length); // copy of array a
         
-        boolean flag = Arrays.equals(a, b);
+        boolean flag = Arrays.equals(a, bOne);
         System.out.println("Arrays are equal: " + flag); // checking if two arrays are equal
 
-        Arrays.sort(b);
+        Arrays.sort(bOne);
 
         // for each loop
         for (int i:a) {
             System.out.print(i + " "); // output
         }
+        
         System.out.println();
         // multi-dimensional arrays
-        // int[][] b = new int[3][4]; // declatation and instantiation
+        int[][] b = new int[3][4]; // declatation and instantiation
 
-        // for (int i = 0; i < b.length; i++) {
-        //     for (int j = 0; j < b[i].length; j++) {
-        //         b[i][j] = (i+1) * (j+1); // initialization
-        //     }
-        // }
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                b[i][j] = (i+1) * (j+1); // initialization
+            }
+        }
 
-        // for (int i = 0; i < b.length; i++) {
-        //     for (int j = 0; j < b[i].length; j++) {
-        //         System.out.print(b[i][j] + " "); // output
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                System.out.print(b[i][j] + " "); // output
+            }
+            System.out.println();
+        }
 
         sc.close(); // close scanner
     }
